caad7ec6651092773651337d6c16e6f0c8c8c5b7
WitalyK <o211ntrrrtt@gmail.com>
2020-11-12 22:33:18 +0300

    comb2checkio

--- 123.py
+++ 123.py
@@ -1,5 +1,9 @@
-wins = 0
-a, b = 3, 2
-wins += (a > b)
-print(wins)
-print(a>b)
+
+def is_acceptable_password(password: str) -> bool:
+    return len(password)>6 and any(it.isdigit() for it in password) \
+           and not all(it.isdigit() for it in password)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('984798734839743'))

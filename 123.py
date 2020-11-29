d2574d0cdf06dc76a0e5f0aa3564e8f487e84ac1
WitalyK <o211ntrrrtt@gmail.com>
2020-11-29 06:52:37 +0300

    c

--- 123.py
+++ 123.py
@@ -1,12 +1,6 @@
-import numpy as np
+def mountain_scape(angles: list[tuple]) -> int:
 
-Z = np.zeros((5, 5))
-Z += np.arange(5)
-# Z = np.random.random_integers(0,5,(7,5))
-Z = np.random.randint(0,5,(5,5))
 
 
 if __name__ == '__main__':
-    print(Z)
-    print(np.array(list(np.ndenumerate(Z))))
-    print(Z.argsort())
+    print(mountain_scape([(0, 2), (5, 3), (7, 5)]))
